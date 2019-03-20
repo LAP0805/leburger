d8e995f60a87fcd8859f4d4e3a375e94fe0d867b
Janxxy <lesley.pond@gmail.com>
2019-03-19 23:51:20 -0400

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 const mysql = require("mysql");
 const path= require("path");
-
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  } else {
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "hello9",
     database: "leburger_db"
 });
-
+  }
 connection.connect(function(error){
     if (error){
         throw error;    
